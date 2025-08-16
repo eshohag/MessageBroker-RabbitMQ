41cc672d26b00699a604f7c50bd16dd57932ff82
MD Shohag Mia <eshohag@outlook.com>
2025-08-16 20:17:16 +0600

    Update WeatherForecastController.cs
    Shohag Added Code

--- MessageProducerService/Controllers/WeatherForecastController.cs
+++ MessageProducerService/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ namespace MessageProducerService.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            //Shohag Added Code
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
